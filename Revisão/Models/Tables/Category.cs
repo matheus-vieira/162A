0284193c17a1e3eecb07982de890f61e20a5fab1
Matheus Costa Vieira <matheus.costa.vieira@gmail.com>
2017-08-17 20:35:25 -0300

    Added description property on category

--- Revisão/Models/Tables/Category.cs
+++ Revisão/Models/Tables/Category.cs
@@ -11,6 +11,8 @@ namespace Models.Tables
         public long CategoryId { get; set; }
         public string Name { get; set; }
 
+        public string Description { get; set; }
+
         public ICollection<Product> Products { get; set; }
     }
 }
